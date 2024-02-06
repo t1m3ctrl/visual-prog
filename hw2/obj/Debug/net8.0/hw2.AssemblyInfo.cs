a74f7deebd6d1b2b68912117e8ceb7668df6a921
t1m3ctrl <t1m3ctrl@gmail.com>
2024-02-06 13:15:33 +0700

    add color
    add color to buttons

--- hw2/obj/Debug/net8.0/hw2.AssemblyInfo.cs
+++ hw2/obj/Debug/net8.0/hw2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hw2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d56a1a88ecc8696bdcd6f229b9ed93d71e95ea9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e4e61fc01de3a21e12bf31b82ed1d98384090d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("hw2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hw2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
