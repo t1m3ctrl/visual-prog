70197d644885f75f56ab7d757f88e3faf7e415ba
t1m3ctrl <t1m3ctrl@gmail.com>
2024-02-13 01:53:22 +0700

    init commit
    init commit for calculator layout

--- hw3_calc/hw3_calc/obj/Debug/net8.0/hw3_calc.AssemblyInfo.cs
+++ hw3_calc/hw3_calc/obj/Debug/net8.0/hw3_calc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hw3_calc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+272be3894d24efc5fdd2c8d3abe6a5878f1c5afb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5824e3e53c72f8429a510e227b5cd551596a69ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("hw3_calc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hw3_calc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
